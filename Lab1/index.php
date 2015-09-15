67bd8fa0edbf20889a544911c34909a025a5042b
trailofdad <christian.hapgood@gmail.com>
2015-09-15 14:57:32 -0300

    Lab1 Step 1

---
+++ Lab1/index.php
@@ -0,0 +1,15 @@
+/**
+ * Created by PhpStorm.
+ * User: inet2005
+ * Date: 9/15/15
+ * Time: 2:53 PM
+ */
+
+<?php
+$number1 = 5;
+$number2 = 8;
+$sum = $number1 + $number2;
+echo "<h1>The sum is: ";
+echo $sum;
+echo "</h1>";
+?>
