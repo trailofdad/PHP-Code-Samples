52a0190fe8c3e3f565a30dfa774f62defb086000
trailofdad <christian.hapgood@gmail.com>
2015-09-18 11:24:16 -0300

    Lab1 Step 2 - working on part 2

---
+++ Lab1/PartB.php
@@ -0,0 +1,36 @@
+<html>
+
+<head></head>
+
+<body>
+
+<?php
+
+echo "<h1>Greetings from the lab</h1>";
+
+?>
+
+<p>
+    <?php
+        echo "<h3>BRRRRAAAAAAAPPPP</h3>";
+    ?>
+</p>
+<p>
+    <?php
+        $myName = "Christian";
+        echo "My name is " . $myName;
+    ?>
+</p>
+<p>
+    <?php
+        $stringOne = "This is string one";
+        $stringTwo = "and this is string two.";
+        $concat = $stringOne . " " . $stringTwo;
+        echo $concat;
+    ?>
+</p>
+
+</body>
+
+</html>
+
