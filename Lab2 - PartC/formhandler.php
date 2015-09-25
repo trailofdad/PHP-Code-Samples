1f5a9ef8456352e688369f1e8c9788c16eb449e7
trailofdad <christian.hapgood@gmail.com>
2015-09-25 01:44:37 -0300

    Part C Step 1 Complete

--- Lab2 - PartC/formhandler.php
+++ Lab2 - PartC/formhandler.php
@@ -1,5 +1,5 @@
 <?php
 
 echo "Your first name is: " . $_POST["firstname"] . "<br/>";
-echo "Your last name is: " . $_POST["lastname"];
-//echo "Your height in metres is: " . $_POST[""]
+echo "Your last name is: " . $_POST["lastname"] . "<br/>";
+echo "Your height in metres is: " . (($_POST["feet"]*12) + $_POST["inches"])*0.0254;
