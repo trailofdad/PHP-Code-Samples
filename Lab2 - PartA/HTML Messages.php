0abc3d10f18af55c6ae61020a754f56b1db0a68f
trailofdad <christian.hapgood@gmail.com>
2015-09-25 12:05:04 -0300

    fixed default_mimetype part

--- Lab2 - PartA/HTML Messages.php
+++ Lab2 - PartA/HTML Messages.php
@@ -1,7 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: inet2005
- * Date: 9/24/15
- * Time: 1:28 PM
- */
+
+echo "<h1>This page was rendered in PHP version ". PHP_VERSION . "</h1>";
+echo "<h1>This page uses ZEND VERSION ". zend_version() . "</h1>";
+echo "<h1>default_mimetype value: " . ini_get(default_mimetype) . ":</h1>";
