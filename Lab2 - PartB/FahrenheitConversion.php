568acc9a144e0e10b2e04c65c9d3c6f7734e4fc8
trailofdad <christian.hapgood@gmail.com>
2015-09-25 00:35:02 -0300

    Part B #2

--- Lab2 - PartB/FahrenheitConversion.php
+++ Lab2 - PartB/FahrenheitConversion.php
@@ -1,11 +1,42 @@
-<?php
 
-function conversion(){
 
-    for($i=0;$i<=100;$i++)
-    {
-        print $i . " degree(s) fahrenheit equals " . (($i-32)*(5/9)) . " degree(s) Celsius <br/>";
-    }
-}
+<html>
+<head>
+    <style type="text/css">
+        table,th,td,tr {
+            border: 1px solid black;
+        }
 
-conversion();
+    </style>
+</head>
+<body>
+
+<table>
+    <thead>
+        <tr>
+            <th>Fahrenheit</th>
+            <th>Celsius</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+
+            for($i=0;$i<=100;$i++)
+            {
+                echo "<tr>";
+                echo "<td>$i</td>";
+                $celsius = ($i-32)*(5/9);
+                echo "<td>$celsius</td>";
+                echo "</tr>";
+            }
+        ?>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+    </tbody>
+</table>
+
+
+</body>
+</html>
