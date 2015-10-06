a2e3b3d6c0bd0ed301d825cf63a24540c56e6bec
trailofdad <christian.hapgood@gmail.com>
2015-10-06 13:38:53 -0300

    still need to fix refresh

--- Lab 3 Part C/LAB 3 Part C/scripts.js
+++ Lab 3 Part C/LAB 3 Part C/scripts.js
@@ -26,34 +26,41 @@ function validateForm()
     {
         alert("Name must be filled out");
         document.getElementById("fname").style.border = "2px solid red";
+        return false;
     }
     else if (lastName == 0)
     {
         alert("Last name must be filled out");
         document.getElementById("lname").style.border = "2px solid red";
+        return false;
     }
     else if (addressOne == 0)
     {
         alert("Address One must be filled out");
         document.getElementById("a1").style.border = "2px solid red";
+        return false;
     }
     else if (addressTwo == 0)
     {
         alert("Address Two must be filled out");
         document.getElementById("a2").style.border = "2px solid red";
+        return false;
     }
     else if (email == 0)
     {
         alert("Email must be filled out");
         document.getElementById("email").style.border = "2px solid red";
+        return false;
     }
     else if (check == false)
     {
         alert("You must agree to the terms of service");
         document.getElementById("span").style.display = "block";
+        return false;
     }
     else
     {
         alert("form validated");
+        return true;
     }
 }
