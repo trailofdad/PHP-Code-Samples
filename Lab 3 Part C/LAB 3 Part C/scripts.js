cee1405c76f7e103138cea7272d5d7a48409175b
trailofdad <christian.hapgood@gmail.com>
2015-10-06 10:17:00 -0300

    working on JS functions - add italics and form validation

--- Lab 3 Part C/LAB 3 Part C/scripts.js
+++ Lab 3 Part C/LAB 3 Part C/scripts.js
@@ -4,20 +4,28 @@
 
 "use strict";
 
-document.getElementById("fname").addEventListener("onchange", makeYellow("fname"));
-document.getElementById("lname").addEventListener("onchange", makeYellow("lname"));
-document.getElementById("a1").addEventListener("onchange", makeYellow("a1"));
-document.getElementById("a2").addEventListener("onchange", makeYellow("a2"));
-document.getElementById("email").addEventListener("onchange", makeYellow("email"));
-
-document.getElementById("divOne").addEventListener("click", function(){
-    expandMenu('divOne');
-});
-
+//document.getElementById("fname").addEventListener("focus", makeYellow("fname"));
+//document.getElementById("lname").addEventListener("focus", makeYellow("lname"));
+//document.getElementById("a1").addEventListener("focus", makeYellow("a1"));
+//document.getElementById("a2").addEventListener("focus", makeYellow("a2"));
+//document.getElementById("email").addEventListener("focus", makeYellow("email"));
+//
+//document.getElementById("fname").addEventListener("blur", clearYellow("fname"));
+//document.getElementById("lname").addEventListener("blur", clearYellow("lname"));
+//document.getElementById("a1").addEventListener("blur", clearYellow("a1"));
+//document.getElementById("a2").addEventListener("blur", clearYellow("a2"));
+//document.getElementById("email").addEventListener("blur", clearYellow("email"));
 
 function makeYellow(id)
 {
     document.getElementById(id).style.backgroundColor = "yellow";
+    document.getElementById(id).parentElement.style.textDecoration = "underline";
+}
+
+function clearYellow(id)
+{
+    document.getElementById(id).style.backgroundColor = "white";
+    document.getElementById(id).parentElement.style.textDecoration = "none";
 }
 
 function validateForm()
