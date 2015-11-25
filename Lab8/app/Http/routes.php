5d4f1f2cbe7cd234fd8ed5da8dcd4ac9ddc8d625
trailofdad <christian.hapgood@gmail.com>
2015-11-24 22:59:39 -0400

    up to video 4

--- Lab8/app/Http/routes.php
+++ Lab8/app/Http/routes.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
-
-Route::get('home', 'HomeController@index');
+Route::get('about', 'PagesController@about');
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
---
+++ Lab8/resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>About Page</title>
+
+</head>
+
+<body>
+    <h1>About Page: {{ $first }} {{ $last }}</h1>
+    <p>Herp derpsum derp herpsum herpy pee herderder terpus berp herpler tee merpus. Merp der ter tee, terp ner serp. Derpus derpsum mer der herderder terpus terp dee tee. Mer me derp dee. Ter merpus merp mer nerpy herpsum herpem herp pee ner.</p>
+</body>
+</html>
